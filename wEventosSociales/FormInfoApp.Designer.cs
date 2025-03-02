ee23660059e36e384276f91668e3aafe63433d17
julian1234zippote <julianbp058@gmail.com>
2025-03-01 19:12:42 -0500

    infoappformulario
    eh no probe la conexion e los botones a ver si servia y ahora que caigo en cuenta me falto agregarle arriba del todo el menu ese botones que tiene el cerra mi bad

---
+++ wEventosSociales/FormInfoApp.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace wEventosSociales
+{
+    partial class FormInfoApp
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormInfoApp));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtInfoTareas = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.btnReservarEventos = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(74, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(243, 37);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Lista De Tareas";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(516, 18);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(349, 37);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Agenda De Actividades";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(982, 18);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(312, 37);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Gestion De Invitados";
+            // 
+            // txtInfoTareas
+            // 
+            this.txtInfoTareas.Font = new System.Drawing.Font("Segoe UI Emoji", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtInfoTareas.Location = new System.Drawing.Point(12, 74);
+            this.txtInfoTareas.Multiline = true;
+            this.txtInfoTareas.Name = "txtInfoTareas";
+            this.txtInfoTareas.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtInfoTareas.Size = new System.Drawing.Size(423, 623);
+            this.txtInfoTareas.TabIndex = 3;
+            this.txtInfoTareas.Text = resources.GetString("txtInfoTareas.Text");
+            this.txtInfoTareas.TextChanged += new System.EventHandler(this.txtInfoTareas_TextChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Segoe UI Emoji", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(484, 74);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(423, 623);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.Text = resources.GetString("textBox1.Text");
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Segoe UI Emoji", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(940, 69);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox2.Size = new System.Drawing.Size(423, 623);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.Text = resources.GetString("textBox2.Text");
+            // 
+            // btnReservarEventos
+            // 
+            this.btnReservarEventos.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReservarEventos.Location = new System.Drawing.Point(484, 745);
+            this.btnReservarEventos.Name = "btnReservarEventos";
+            this.btnReservarEventos.Size = new System.Drawing.Size(423, 71);
+            this.btnReservarEventos.TabIndex = 6;
+            this.btnReservarEventos.Text = "Reservar Eventos";
+            this.btnReservarEventos.UseVisualStyleBackColor = true;
+            this.btnReservarEventos.Click += new System.EventHandler(this.btnReservarEventos_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(484, 745);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(74, 71);
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(3, 0);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(1383, 862);
+            this.pictureBox2.TabIndex = 8;
+            this.pictureBox2.TabStop = false;
+            // 
+            // FormInfoApp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1384, 861);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.txtInfoTareas);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnReservarEventos);
+            this.Controls.Add(this.pictureBox2);
+            this.Name = "FormInfoApp";
+            this.Text = "FormInfoEventos";
+            this.Load += new System.EventHandler(this.FormInfoEventos_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtInfoTareas;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button btnReservarEventos;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
---
+++ wEventosSociales/FormInfoApp.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace wEventosSociales
+{
+    public partial class FormInfoApp: Form
+    {
+        public FormInfoApp()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void chart1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FormInfoEventos_Load(object sender, EventArgs e)
+        {
+            
+
+
+
+
+        }
+
+        private void txtInfoTareas_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnReservarEventos_Click(object sender, EventArgs e)
+        {
+            FormInfoApp Form1 = new FormInfoApp();
+            Form1.Show();
+            this.Close();
+        }
+    }
+}
